eae9954b17eb1e20a7b7e409e740d0942e9e037d
bp-stripa006 <stripathi1@xavient.com>
2016-08-16 10:32:20 +0530

    updated

--- src/main/java/com/core/connector/rest/RestClient.java
+++ src/main/java/com/core/connector/rest/RestClient.java
@@ -9,36 +9,40 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 /**
- * A simple wrapper around Spring's RestTemplate to allow for easy creation with V4's instrumentation.  This is available for edge-case needs.  You should use {@link RestClientFactory} to get your client if at all possible.
+ * A simple wrapper around Spring's RestTemplate to allow for easy creation with
+ * V4's instrumentation. This is available for edge-case needs. You should use
+ * {@link RestClientFactory} to get your client if at all possible.
  * 
- * Use of this class does not guarantee the availability of timeout functionality. 
+ * Use of this class does not guarantee the availability of timeout
+ * functionality.
  */
 @Component
 public class RestClient extends RestTemplate {
 
-    /**
-     * 
-     */
-    public RestClient() {
-        super();
-    }
-    
-    public RestClient(ClientHttpRequestFactory requestFactory) {
-        super(requestFactory);
-    }
+	/**
+	 * 
+	 */
+	public RestClient() {
+		super();
+	}
 
-    public RestClient(List<ClientHttpRequestInterceptor> interceptors) {
-        super();
-        setInterceptors(interceptors);
-    }
+	public RestClient(ClientHttpRequestFactory requestFactory) {
+		super(requestFactory);
+	}
 
-    public RestClient(ClientHttpRequestFactory requestFactory, List<ClientHttpRequestInterceptor> interceptors) {
-        super(requestFactory);
-        setInterceptors(interceptors);
-    }
+	public RestClient(List<ClientHttpRequestInterceptor> interceptors) {
+		super();
+		setInterceptors(interceptors);
+	}
 
-    public RestClient(List<HttpMessageConverter<?>> messageConverters, List<ClientHttpRequestInterceptor> interceptors) {
-        super(messageConverters);
-        setInterceptors(interceptors);
-    }
+	public RestClient(ClientHttpRequestFactory requestFactory, List<ClientHttpRequestInterceptor> interceptors) {
+		super(requestFactory);
+		setInterceptors(interceptors);
+	}
+
+	public RestClient(List<HttpMessageConverter<?>> messageConverters,
+			List<ClientHttpRequestInterceptor> interceptors) {
+		super(messageConverters);
+		setInterceptors(interceptors);
+	}
 }
